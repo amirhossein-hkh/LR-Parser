222ce97b211343f142e8313137d2cf6e7e17e863
Giacomo <Cau.Giacomo190@Tiscali.it>
2021-09-22 11:16:38 +0200

    Removed commented code

--- src/lr1/LR1Parser.java
+++ src/lr1/LR1Parser.java
@@ -2,7 +2,6 @@ package lr1;
 
 import static util.Grammar.EndToken;
 
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 import util.Grammar;
@@ -30,16 +29,11 @@ public class LR1Parser extends LRParser<LR1State, LR1Item> {
 		for (int i=0; i<statesList.size(); i+=1) {
 			var statei = statesList.get(i);
 			var itemsi = statei.getItems();
-			//var itemsiLR0 = itemsi.stream().map(LR0Item::new).collect(toSet());
 			for (int j=statesList.size()-1; j>i; j-=1) {
 				var itemsj = statesList.get(j).getItems();
-				//var itemsjLR0 = itemsj.stream().map(LR0Item::new).collect(toSet());
-				//if (!itemsiLR0.equals(itemsjLR0)) continue;
 				if (!equalsLR0(itemsi, itemsj)) continue;
-				//if (!((SetLR1Item) itemsi).equalsLR0(itemsj)) continue;
 				itemsi.forEach(iti-> iti.lookahead.addAll(itemsj.stream().filter(itj-> itj.equalsLR0(iti)).findFirst().get().lookahead));
 				statesList.stream().map(LR1State::getTransitions).forEach(ts-> ts.forEach((s,st)->{ if (st.getItems().equals(itemsj)) ts.put(s,statei); }));
-				//statesList.stream().map(LR1State::getTransitions).forEach(ts-> ts.entrySet().stream().filter(e->e.getValue().getItems().equals(itemsj)).map(e->ts.put(e.getKey(),statei)));
 				statesList.remove(j);
 			}
 			newStateList.add(statei);
@@ -47,19 +41,17 @@ public class LR1Parser extends LRParser<LR1State, LR1Item> {
 		statesList = newStateList;
 	}
 	
-	//*
 	private boolean equalsLR0(Set<LR1Item> itemsi, Set<LR1Item> itemsj) {
         if (itemsi == itemsj) return true;
         if (itemsi.size() != itemsj.size()) return false;
-        itemsj = new LinkedHashSet(itemsj);
+        //itemsj = new HashSet(itemsj);
         loop: for (LR1Item itemi: itemsi) {
         	for (LR1Item item2: itemsj) if (itemi.equalsLR0(item2)) {
-        		itemsj.remove(item2);
+        		//itemsj.remove(item2);
         		continue loop;
         	}
          	return false;
     	}
         return true;
 	}
-	//*/
 }
